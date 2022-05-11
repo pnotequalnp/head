6edc66a1119646433033f132c1c3546a940dc9d4
Kevin Mullins <kevin@pnotequalnp.com>
2022-05-10 20:18:36 -0700

    Match coreutils better

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,57 @@
 module Main (main) where
 
-import Control.Monad (unless)
+import Control.Monad (unless, when)
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 (pack)
 import Data.Foldable (for_)
+import Data.Version (showVersion)
 import Lens.Family (view)
 import Options (Options (..), getOptions)
+import Paths_head qualified as Paths
 import Pipes
-import Pipes.ByteString (fromHandle, splitOn, stdin, stdout)
+import Pipes.ByteString (fromHandle, splitOn, stdin, stdout, take)
 import Pipes.Group (FreeT, intercalates, takes)
 import System.Exit (die)
 import System.IO (IOMode (..), openFile)
 import System.IO.Error (isDoesNotExistError, isPermissionError, tryIOError)
+import Prelude hiding (take)
 
 main :: IO ()
 main = do
-  Options {count, delim, files, quiet} <- getOptions
+  Options {count, delim, files, quiet, verbose, version} <- getOptions
 
-  let source = case files of
-        [] -> takeLines count delim stdin
-        _ -> for_ files \filepath -> do
-          unless quiet do
+  when version do
+    die (showVersion Paths.version)
+
+  let op = case count of
+        Left n -> takeBytes n
+        Right n -> takeLines n delim
+      source = case files of
+        [] -> do
+          when verbose do
+            yield "==> standard input <==\n"
+          op stdin
+        ["-"] -> do
+          when verbose do
+            yield "==> standard input <==\n"
+          op stdin
+        [filepath] -> do
+          when verbose do
             yield ("==> " <> pack filepath <> " <==\n")
-          file <- lift (openHandle filepath)
-          takeLines count delim file
+          lift (openHandle filepath) >>= op
+        _ -> for_ files \filepath -> do
+          let fp = case filepath of
+                "-" -> "standard input"
+                _ -> pack filepath
+          unless (quiet && not verbose) do
+            yield ("==> " <> fp <> " <==\n")
+          lift (openHandle filepath) >>= op
           yield "\n"
 
   runEffect (source >-> stdout)
 
 lines' :: Monad m => ByteString -> Producer ByteString m a -> FreeT (Producer ByteString m) m a
-lines' d = \s -> view (splitOn d) s -- This is eta expanded to help GHC with type inference
+lines' d = view (splitOn d)
 
 unlines' :: Monad m => ByteString -> FreeT (Producer ByteString m) m a -> Producer ByteString m a
 unlines' = intercalates . yield
@@ -39,8 +61,10 @@ takeLines n sep s = do
   (unlines' sep . takes n . lines' sep) s
   yield sep
 
+takeBytes :: Monad m => Int -> Producer ByteString m () -> Producer ByteString m ()
+takeBytes n = (>-> take n)
+
 openHandle :: MonadIO m => String -> IO (Producer ByteString m ())
-openHandle "-" = pure stdin
 openHandle filepath =
   tryIOError (openFile filepath ReadMode) >>= \case
     Right h -> pure (fromHandle h)
--- app/Options.hs
+++ app/Options.hs
@@ -1,29 +1,67 @@
 module Options (Options (..), getOptions) where
 
 import Data.ByteString (ByteString)
+import Data.Foldable (fold)
 import Options.Applicative
+import Options.Applicative.Help (vsep)
 
 data Options = Options
-  { count :: !Int
+  { count :: !(Either Int Int)
   , delim :: !ByteString
-  , files :: ![FilePath]
   , quiet :: !Bool
+  , verbose :: !Bool
+  , version :: !Bool
+  , files :: ![FilePath]
   }
 
 getOptions :: IO Options
-getOptions = execParser (info (helper <*> parser) fullDesc)
+getOptions = execParser (info (helper <*> parser) (fullDesc <> progDescDoc (Just desc)))
+  where
+    desc =
+      vsep
+        [ "Print the first 10 lines of each FILE to standard output.\n"
+        , "With more than one FILE, precede each with a header giving the file name."
+        , "With no FILE, or when FILE is -, read standard input."
+        ]
 
 parser :: Parser Options
-parser = Options <$> parseCount <*> parseDelim <*> parseFiles <*> parseQuiet
+parser = Options <$> parseCount <*> parseDelim <*> parseQuiet <*> parseVerbose <*> parseVersion <*> parseFiles
 
-parseCount :: Parser Int
-parseCount = option auto (short 'n' <> long "lines" <> value 10 <> metavar "NUM")
+parseLines :: Parser Int
+parseLines =
+  option auto $
+    fold
+      [ short 'n'
+      , long "lines"
+      , value 10
+      , metavar "NUM"
+      , help "Print the first NUM lines instead of the first 10"
+      ]
+
+parseBytes :: Parser Int
+parseBytes =
+  option auto $
+    fold
+      [ short 'c'
+      , long "bytes"
+      , metavar "NUM"
+      , help "Print the first NUM bytes of each file"
+      ]
+
+parseCount :: Parser (Either Int Int)
+parseCount = Left <$> parseBytes <|> Right <$> parseLines
 
 parseDelim :: Parser ByteString
-parseDelim = flag "\n" "\0" (short 'z' <> long "zero-terminated")
+parseDelim = flag "\n" "\0" (short 'z' <> long "zero-terminated" <> help "Line delimiter is NUL, not newline")
 
 parseFiles :: Parser [FilePath]
 parseFiles = many (strArgument (metavar "FILE"))
 
 parseQuiet :: Parser Bool
-parseQuiet = switch (short 'q' <> long "quiet" <> long "silent")
+parseQuiet = switch (short 'q' <> long "quiet" <> long "silent" <> help "Never print headers giving file names")
+
+parseVerbose :: Parser Bool
+parseVerbose = switch (short 'v' <> long "verbose" <> help "Always print headers giving file names")
+
+parseVersion :: Parser Bool
+parseVersion = switch (long "version" <> help "Output version information and exit")
